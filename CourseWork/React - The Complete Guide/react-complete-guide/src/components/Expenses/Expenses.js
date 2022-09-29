274212784be54470892e2b6c7e2f6b5eca84a06d
meharpreet-bst <meharpreet@bluestacks.com>
2022-09-30 00:06:09 +0530

    feat(react): Assignment 3 solved
    Update code to add functionality for filters
    Selecting a filter value must display only those expenses which were
    listed in that particular year

--- CourseWork/React - The Complete Guide/react-complete-guide/src/components/Expenses/Expenses.js
+++ CourseWork/React - The Complete Guide/react-complete-guide/src/components/Expenses/Expenses.js
@@ -11,17 +11,21 @@ const Expenses = (props) => {
         setFilteredYear(selectedYear);
     };
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
     return (
         <Card className="expenses">
             <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-            {props.items.map(
+            {filteredExpenses.map(
                 expense => <ExpenseItem 
                     key={expense.id}
                     title={expense.title} 
                     amount={expense.amount} 
                     date={expense.date}/>
                 )
-            };
+            }
         </Card>
     );
 }
