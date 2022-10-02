75dab08e659105d8d910ba7131acaad0bd83b29a
meharpreet-bst <meharpreet@bluestacks.com>
2022-10-02 23:02:36 +0530

    feat(react): Assignment 4 Solved
    Added consitional formatting to Show/Hide New Expense Form on Button Click
    Added a button `Add New Expense`, clicking which displays the Add new
    expense form. If not clicked the form stays hidden

--- CourseWork/React - The Complete Guide/react-complete-guide/src/components/Expenses/ExpensesList.js
+++ CourseWork/React - The Complete Guide/react-complete-guide/src/components/Expenses/ExpensesList.js
@@ -1,5 +1,7 @@
 import ExpenseItem from "./ExpenseItem";
 
+import './ExpensesList.css';
+
 const ExpensesList = props => {
     if (props.items.length === 0) {
         return <h2 className="expenses-list__fallback">
