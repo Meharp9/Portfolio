069ec5ff7bbc97a7ccdf8180961862dab6331cae
meharpreet-bst <meharpreet@bluestacks.com>
2022-10-02 23:20:40 +0530

    feat(react): Added a chart to expense tracker

--- CourseWork/React - The Complete Guide/react-complete-guide/src/components/Expenses/Expenses.js
+++ CourseWork/React - The Complete Guide/react-complete-guide/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import ExpenseFilter from "./ExpenseFilter";
 import './Expenses.css'
 import { useState } from "react";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
@@ -18,6 +19,7 @@ const Expenses = (props) => {
     return (
         <Card className="expenses">
             <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+            <ExpensesChart expenses={filteredExpenses}/>
             <ExpensesList items={filteredExpenses} />
         </Card>
     );
